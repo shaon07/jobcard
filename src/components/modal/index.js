9d44ff0000ef6984fa6ad655775785d36bc8adf1
shaon07 <alishaon078@gmail.com>
2022-11-28 17:09:54 +0600

    fixed modal responsive size

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -7,14 +7,16 @@ import styles from './modal.module.css';
 
 const style = {
   position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: "50%",
+  top: "2rem",
+  left: 0,
+  right: 0,
+  width: "90%",
+  maxWidth: "700px",
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
-  p: 4,
+  margin: "auto",
+  display: "block"
 };
 
 export default function BasicModal() {
@@ -41,127 +43,127 @@ export default function BasicModal() {
   }
 
   return (
-    <div>
-      <Modal
-        open={modalOpen}
-        onClose={handleCloseModal}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <form className={`${styles.modalForm}`} onSubmit={handleSubmit}>
-            <code style={{ color: "red", marginBottom: "20px" }}>{error && JSON.stringify(error)}</code>
-            <div className={`${styles.modalEmail} ${styles.inputBox}`}>
-              <label htmlFor="text">Job Title</label>
-              <input
-                type="text"
-                id="text"
-                name="jobTitle"
-                required
-                value={userInfo.jobTitle}
-                onChange={handleChange}
-              />
-            </div>
-            <div className={`${styles.modalEmail} ${styles.inputBox}`}>
-              <label htmlFor="text">level</label>
-              <input
-                type="text"
-                id="text"
-                name="level"
-                required
-                value={userInfo.level}
-                onChange={handleChange}
-              />
-            </div>
-            <div className={`${styles.modalEmail} ${styles.inputBox}`}>
-              <label htmlFor="lastDateOfApply">last Date OfApply</label>
-              <input
-                type="date"
-                id="lastDateOfApply"
-                name="lastDateOfApply"
-                required
-                value={userInfo.lastDateOfApply}
-                onChange={handleChange}
-              />
-            </div>
-            <div className={`${styles.modalEmail} ${styles.inputBox}`}>
-              <label htmlFor="shift">{userInfo.shift || "Select gender"}</label>
-              <select
-                name="shift"
-                value={userInfo.shift || "Select shift"}
-                onChange={(e) => setUserInfo({ ...userInfo, shift: e.target.value })}
-              >
-                <option value="Select">Select</option>,
-                <option value="day">Day</option>,
-                <option value="night">Night</option>,
-              </select>
-            </div>
-            <div className={`${styles.modalPassword} ${styles.inputBox}`}>
-              <label htmlFor="location">location</label>
-              <input
-                type="text"
-                id="location"
-                name="location"
-                required
-                value={userInfo.location}
-                onChange={handleChange}
-              />
-            </div>
-            <div className={`${styles.modalPassword} ${styles.inputBox}`}>
-              <label htmlFor="vacancies">vacancies</label>
-              <input
-                type="number"
-                id="vacancies"
-                name="vacancies"
-                required
-                value={userInfo.vacancies}
-                onChange={handleChange}
-              />
-            </div>
 
-            <div className={`${styles.modalEmail} ${styles.inputBox}`}>
-              <label htmlFor="jobType">{userInfo.jobType || "Select jobType"}</label>
-              <select
-                name="jobType"
-                value={userInfo.jobType || "Select jobType"}
-                onChange={(e) => setUserInfo({ ...userInfo, jobType: e.target.value })}
-              >
-                <option value="Select">Select</option>,
-                <option value="full_time">full time</option>,
-                <option value="part_time">part time</option>,
-                <option value="internship">internship</option>,
-              </select>
-            </div>
+    <Modal
+      open={modalOpen}
+      onClose={handleCloseModal}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <form className={`${styles.modalForm}`} onSubmit={handleSubmit}>
+          <code style={{ color: "red", marginBottom: "20px" }}>{error && JSON.stringify(error)}</code>
+          <div className={`${styles.modalEmail} ${styles.inputBox}`}>
+            <label htmlFor="text">Job Title</label>
+            <input
+              type="text"
+              id="text"
+              name="jobTitle"
+              required
+              value={userInfo.jobTitle}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={`${styles.modalEmail} ${styles.inputBox}`}>
+            <label htmlFor="text">level</label>
+            <input
+              type="text"
+              id="text"
+              name="level"
+              required
+              value={userInfo.level}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={`${styles.modalEmail} ${styles.inputBox}`}>
+            <label htmlFor="lastDateOfApply">last Date OfApply</label>
+            <input
+              type="date"
+              id="lastDateOfApply"
+              name="lastDateOfApply"
+              required
+              value={userInfo.lastDateOfApply}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={`${styles.modalEmail} ${styles.inputBox}`}>
+            <label htmlFor="shift">{userInfo.shift || "Select gender"}</label>
+            <select
+              name="shift"
+              value={userInfo.shift || "Select shift"}
+              onChange={(e) => setUserInfo({ ...userInfo, shift: e.target.value })}
+            >
+              <option value="Select">Select</option>,
+              <option value="day">Day</option>,
+              <option value="night">Night</option>,
+            </select>
+          </div>
+          <div className={`${styles.modalPassword} ${styles.inputBox}`}>
+            <label htmlFor="location">location</label>
+            <input
+              type="text"
+              id="location"
+              name="location"
+              required
+              value={userInfo.location}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={`${styles.modalPassword} ${styles.inputBox}`}>
+            <label htmlFor="vacancies">vacancies</label>
+            <input
+              type="number"
+              id="vacancies"
+              name="vacancies"
+              required
+              value={userInfo.vacancies}
+              onChange={handleChange}
+            />
+          </div>
 
-            <div className={`${styles.modalPassword} ${styles.inputBox}`}>
-              <label htmlFor="jobDescription">jobDescription</label>
-              <textarea
-                type="number"
-                id="jobDescription"
-                name="jobDescription"
-                required
-                value={userInfo.jobDescription}
-                onChange={handleChange}></textarea>
-            </div>
+          <div className={`${styles.modalEmail} ${styles.inputBox}`}>
+            <label htmlFor="jobType">{userInfo.jobType || "Select jobType"}</label>
+            <select
+              name="jobType"
+              value={userInfo.jobType || "Select jobType"}
+              onChange={(e) => setUserInfo({ ...userInfo, jobType: e.target.value })}
+            >
+              <option value="Select">Select</option>,
+              <option value="full_time">full time</option>,
+              <option value="part_time">part time</option>,
+              <option value="internship">internship</option>,
+            </select>
+          </div>
 
-            <div className={`${styles.modalEmail} ${styles.inputBox}`}>
-              <label htmlFor="department">department</label>
-              <input
-                type="text"
-                id="text"
-                name="department"
-                required
-                value={userInfo.department}
-                onChange={handleChange}
-              />
-            </div>
+          <div className={`${styles.modalPassword} ${styles.inputBox}`}>
+            <label htmlFor="jobDescription">jobDescription</label>
+            <textarea
+              type="number"
+              id="jobDescription"
+              name="jobDescription"
+              required
+              value={userInfo.jobDescription}
+              onChange={handleChange}></textarea>
+          </div>
 
-            <div className={`${styles.formBtn}`}>
-              <Button color="success" type="submit" variant="contained">Submit</Button>
-            </div>
-          </form>
-        </Box>
-      </Modal>
-    </div>
+          <div className={`${styles.modalEmail} ${styles.inputBox}`}>
+            <label htmlFor="department">department</label>
+            <input
+              type="text"
+              id="text"
+              name="department"
+              required
+              value={userInfo.department}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className={`${styles.formBtn}`}>
+            <Button color="success" type="submit" variant="contained">Submit</Button>
+          </div>
+        </form>
+      </Box>
+    </Modal>
+
   );
 }
--- src/components/postDetail/index.js
+++ src/components/postDetail/index.js
@@ -8,49 +8,49 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: "50%",
+  width: "90%",
+  maxWidth: "700px",
   bgcolor: 'background.paper',
   border: '2px solid #000',
   p: 4,
+
 };
 
 export default function PostDetail({ data, postDetailOpen, handlePostDetailClose }) {
   console.log(data, " shon")
   return (
-    <div>
-      <Modal
-        open={postDetailOpen}
-        onClose={handlePostDetailClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h4" component="h2">
-            {data.jobTitle} - {data.jobType}
-          </Typography>
-          <Typography id="modal-modal-title" variant="p" sx={{ mt: 1 }} component="p">
-            Posted : {data.postDate}
-          </Typography>
-          <Typography id="modal-modal-title" variant="p" sx={{ mt: 1 }} component="p">
-            Level : {data.level}
-          </Typography>
-          <Typography id="modal-modal-title" variant="p" sx={{ mt: 1 }} component="p">
-            Shift : {data.shift}
-          </Typography>
-          <Typography id="modal-modal-title" variant="p" sx={{ mt: 1 }} component="p">
-            Location : {data.location}
-          </Typography>
-          <Typography id="modal-modal-title" variant="p" sx={{ mt: 1 }} component="p">
-            vacancies : {data.vacancies}
-          </Typography>
-          <Typography id="modal-modal-title" variant="p" sx={{ mt: 1 }} component="p">
-            Deadline : {data.lastDateOfApply}
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 1 }}>
-            {data.jobDescription}
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
+    <Modal
+      open={postDetailOpen}
+      onClose={handlePostDetailClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h4" component="h2">
+          {data.jobTitle} - {data.jobType}
+        </Typography>
+        <Typography id="modal-modal-title" variant="p" sx={{ mt: 1 }} component="p">
+          Posted : {data.postDate}
+        </Typography>
+        <Typography id="modal-modal-title" variant="p" sx={{ mt: 1 }} component="p">
+          Level : {data.level}
+        </Typography>
+        <Typography id="modal-modal-title" variant="p" sx={{ mt: 1 }} component="p">
+          Shift : {data.shift}
+        </Typography>
+        <Typography id="modal-modal-title" variant="p" sx={{ mt: 1 }} component="p">
+          Location : {data.location}
+        </Typography>
+        <Typography id="modal-modal-title" variant="p" sx={{ mt: 1 }} component="p">
+          vacancies : {data.vacancies}
+        </Typography>
+        <Typography id="modal-modal-title" variant="p" sx={{ mt: 1 }} component="p">
+          Deadline : {data.lastDateOfApply}
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 1 }}>
+          {data.jobDescription}
+        </Typography>
+      </Box>
+    </Modal>
   );
 }
