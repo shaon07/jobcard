e8885b141afa3f3dc5f3180fa2b9ac184cfbe3aa
shaon07 <alishaon078@gmail.com>
2022-11-28 17:28:48 +0600

    added 404 page

--- src/routes/Public.js
+++ src/routes/Public.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom"
+import Invalid from '../components/Invalid'
 import Login from "../components/Login"
 import Register from "../components/Login/register"
 
@@ -8,6 +9,7 @@ export default function Public() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Invalid />} />
     </Routes>
   )
 }
